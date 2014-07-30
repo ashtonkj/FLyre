a34c262909632a40c23e40da643e3bdfdc1722fd
Kevin Ashton <zaowuya@gmail.com>
2014-07-30 19:00:31 +0200

    Initial Commit of Flyre

---
+++ src/FLyre/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FLyre")>]
+[<assembly: AssemblyProductAttribute("FLyre")>]
+[<assembly: AssemblyDescriptionAttribute("A chat prototype")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
